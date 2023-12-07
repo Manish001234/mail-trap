faa464a30141b4e19e28c19d1f46c28585d6409b
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2023-12-07 10:04:01 +0530

    added

--- emailService.js
+++ emailService.js
@@ -1,25 +1,25 @@
-// emailService.js
+
 const nodemailer = require('nodemailer');
 
-// Create a transporter with your email service provider's settings
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'manish@mushroomworldbpl.com',    // replace with your Gmail email address
-    pass: 'abcd1234',     // replace with your Gmail password or an app-specific password
+    user: 'manish@gmail.com',    // replace with your Gmail email address
+    pass: 'abcd134',     
   },
 });
 
-// Function to send an email
+
 const sendEmail = (to, subject, text) => {
   const mailOptions = {
-    from: 'manish@mushroomworldbpl.com',   // replace with your Gmail email address
+    from: 'manish@gmail.com',   
     to,
     subject,
     text,
   };
 
-  // Send the email
+
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,16 +9,16 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-// Connect to MongoDB (replace 'your_mongo_db_connection_string' with your actual connection string)
+
 mongoose.connect('mongodb+srv://manish:reddy@cluster0.bwygebg.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Create a simple MongoDB model
+
 const Operation = mongoose.model('Operation', { data: String });
 
-// API endpoint to perform an operation
+
 app.post('/api/performOperation', async (req, res) => {
   try {
-    // Save operation data to MongoDB
+ 
     const { data } = req.body;
     const operation = new Operation({ data });
     await operation.save();
@@ -26,7 +26,7 @@ app.post('/api/performOperation', async (req, res) => {
     // Send email
     const subject = 'Operation Performed';
     const text = `Operation data: ${data}`;
-    sendEmail('manish@mushroomworldbpl.com', subject, text);
+    sendEmail('manish@gmail.com.com', subject, text);
 
     res.status(200).json({ success: true, message: 'Operation successful' });
   } catch (error) {
